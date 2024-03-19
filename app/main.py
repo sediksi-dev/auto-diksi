2d98ce3358bd251e0fcf6e45a4b97ec19542065a
Muhamad Erza Wansyah <erzawansyah@gmail.com>
2024-03-19 08:00:08 +0700

    A bit changes

--- app/main.py
+++ app/main.py
@@ -12,20 +12,4 @@ auth_password = os.environ.get("SUPABASE_AUTH_PASSWORD")
 
 app = FastAPI()
 
-
-# @app.middleware("http")
-# async def add_process_time_header(request: Request, call_next):
-#     response = await call_next(request)
-#     print("From middleware", request.url.path)
-
-#     if request.url.path.startswith("/bot"):
-#         passkey = request.headers.get("X-AGC-PASSKEY")
-#         if not passkey or passkey != f"{auth_email}:{auth_password}":
-#             response.headers["X-AGC-PASSKEY"] = "not allowed"
-#         else:
-#             response.headers["X-AGC-PASSKEY"] = "allowed"
-
-#     return response
-
-
 app.include_router(bot.router)
