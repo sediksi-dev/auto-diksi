cf43d529bc520220842fd5b06c2942e100ae87d8
Muhamad Erza Wansyah <erzawansyah@gmail.com>
2024-03-19 08:58:27 +0700

    Fix auth env requirements

--- app/main.py
+++ app/main.py
@@ -1,14 +1,5 @@
-import os
 from fastapi import FastAPI
 from app.bot import routers as bot
-from dotenv import load_dotenv
-
-# from modules.supabase.db import db
-
-load_dotenv()
-
-auth_email = os.environ.get("SUPABASE_AUTH_EMAIL")
-auth_password = os.environ.get("SUPABASE_AUTH_PASSWORD")
 
 app = FastAPI()
 
